513c3443ed089a1f0501986f9db3e49aaa747455
Ashutosh4136 <rathashutosh11@gmail.com>
2026-02-06 20:36:37 +0530

    update the urls of vendor

--- vendor/vendor/urls.py
+++ vendor/vendor/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
-    path('categories/', include('categories.urls')),
     path('categories/', include('categories.urls')),
     path('adminpanel/', include('adminpanel.urls')),
     path('products/', include('products.urls')),
@@ -29,6 +31,8 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path('', include('orders.urls')),
     path('reviews/', include('reviews.urls')),
-
-
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
